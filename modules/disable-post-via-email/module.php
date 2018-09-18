34d2251dd8afeecf0a18820a49acb3db6b0051b9
Pau <pauiglesias@gmail.com>
2018-09-18 09:59:10 +0200

    Initial files cloning structure of Speed Demon plugin.

---
+++ modules/disable-post-via-email/module.php
@@ -0,0 +1,39 @@
+<?php
+
+// Subpackage namespace
+namespace LittleBizzy\SecurityGuard\Modules\Disable_Post_Via_Email;
+
+// Aliased namespaces
+use \LittleBizzy\SecurityGuard\Helpers;
+
+/**
+ * Module class
+ *
+ * @package Security Guard
+ * @subpackage Disable Post Via Email
+ */
+class Module extends Helpers\Module {
+
+
+
+	/**
+	 * Module constants
+	 */
+	const FILE = __FILE__;
+	const PREFIX = 'dpveml';
+	const MODULE_NAMESPACE = __NAMESPACE__;
+
+
+
+	/**
+	 * Run the core module
+	 */
+	protected function onConstruct() {
+		if ($this->enabled()) {
+			Core\Core::instance($this);
+		}
+	}
+
+
+
+}
---
+++ modules/disable-xml-rpc/module.php
@@ -0,0 +1,53 @@
+<?php
+
+// Subpackage namespace
+namespace LittleBizzy\SecurityGuard\Modules\Disable_Xml_Rpc;
+
+// Aliased namespaces
+use \LittleBizzy\SecurityGuard\Helpers;
+
+/**
+ * Module class
+ *
+ * @package Security Guard
+ * @subpackage Disable XML-RPC
+ */
+class Module extends Helpers\Module {
+
+
+
+	/**
+	 * Module constants
+	 */
+	const FILE = __FILE__;
+	const PREFIX = 'dsbxml';
+	const MODULE_NAMESPACE = __NAMESPACE__;
+
+
+
+	/**
+	 * Wait to start on WP init hook
+	 */
+	protected function onConstruct() {
+		add_action('init', [$this, 'init'], PHP_INT_MAX);
+	}
+
+
+
+	/**
+	 * Start the module functionality
+	 */
+	public function init() {
+
+		// Last minute check
+		if (!$this->enabled()) {
+			return;
+		}
+
+		// Start
+		new Core\XMLRPC;
+	}
+
+
+
+}
